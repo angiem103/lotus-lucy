4866036de033d738f78ea2064084df8010260a16
angie <amaticorena103@hotmail.com>
2023-01-05 00:13:27 -0500

    added state to sign up form

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+
 
 
 function SignUp () {
@@ -9,8 +10,9 @@ function SignUp () {
         backgroundColor: 'rgb(30, 31, 31)',
         width: '50%',
         color: 'white',
-        padding: '10px'
-    }
+        padding: '10px',
+        borderRadius: '20px'
+    };
 
     const headerStyle = {
         color: 'white',
@@ -21,29 +23,48 @@ function SignUp () {
         fontSize: '50px',
     };
 
+    const [name, setName] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        const customer = {
+            name: name,
+            phone_number: phoneNumber,
+            email: email,
+            username: username,
+            password: password
+        }
+        console.log(customer)
+    }
+
+
     return (
-        <div  class="row h-100 justify-content-center align-items-center">
+        <div  className="row h-100 justify-content-center align-items-center">
         <h1 style={headerStyle}>SIGN UP</h1>
-        <Form style={formStyle}>
+        <Form style={formStyle} onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicName">
             <Form.Label>Name</Form.Label>
-            <Form.Control type="name" placeholder="Name" />
+            <Form.Control type="name" placeholder="Name" onChange={(e) => setName(e.target.value)}/>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPhoneNumber">
             <Form.Label>Phone Number</Form.Label>
-            <Form.Control type="phone number" placeholder="Phone Number" />
+            <Form.Control type="phone number" placeholder="Phone Number"  onChange={(e) => setPhoneNumber(e.target.value)}/>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control type="email" placeholder="Enter email"onChange={(e) => setEmail(e.target.value)} />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicUsername">
             <Form.Label>Username</Form.Label>
-            <Form.Control type="username" placeholder="Username" />
+            <Form.Control type="username" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
           </Form.Group>
           <Button variant="dark" type="submit">
             Submit
