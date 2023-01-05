b3ac3c10a1130ece20967e980719029784439e6d
angie <amaticorena103@hotmail.com>
2023-01-05 00:05:12 -0500

    added form fields

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -7,29 +7,45 @@ function SignUp () {
 
     const formStyle = {
         backgroundColor: 'rgb(30, 31, 31)',
-        width: '60%',
-
+        width: '50%',
+        color: 'white',
+        padding: '10px'
     }
+
+    const headerStyle = {
+        color: 'white',
+        height: '150px',
+        textAlign: 'center',
+        paddingTop: '50px',
+        fontFamily: 'andale mono, monospace',
+        fontSize: '50px',
+    };
 
     return (
         <div  class="row h-100 justify-content-center align-items-center">
+        <h1 style={headerStyle}>SIGN UP</h1>
         <Form style={formStyle}>
+        <Form.Group className="mb-3" controlId="formBasicName">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="name" placeholder="Name" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPhoneNumber">
+            <Form.Label>Phone Number</Form.Label>
+            <Form.Control type="phone number" placeholder="Phone Number" />
+          </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
           </Form.Group>
-    
+          <Form.Group className="mb-3" controlId="formBasicUsername">
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="username" placeholder="Username" />
+          </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
             <Form.Control type="password" placeholder="Password" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
+          <Button variant="dark" type="submit">
             Submit
           </Button>
         </Form>
