3888ca6f72a0486535159235d19d580365f6e556
angie <amaticorena103@hotmail.com>
2023-01-07 21:22:25 -0500

    add events to edit cart form

--- client/src/components/DetailCard.js
+++ client/src/components/DetailCard.js
@@ -1,21 +1,30 @@
 import React, { useState } from "react";
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
+import Button from "react-bootstrap/esm/Button";
+import { FiTrash2 } from "react-icons/fi";
 
 function DetailCard( {details} ) {
 
     const [updatedQty, setUpdatedQty] = useState(details.quantity)
-    console.log(updatedQty)
+    const [updatedDetail, setUpdatedDetail] = useState("hi")
+
+    function handleDetailDelete(){
+        setUpdatedDetail()
+        console.log(updatedDetail)
+    }
 
     return (
+        <div>
         <Form.Group className="mb-3" controlId="formBasicName">
             <Form.Label>{details.product.name} </Form.Label>
-            <img src={details.product.image} />
             <InputGroup className="mb-3" >
                 <InputGroup.Text>Quantity</InputGroup.Text>
                 <Form.Control defaultValue={updatedQty} onChange={ (e) => setUpdatedQty(e.target.value)}/>
             </InputGroup>
+            <Button onClick={handleDetailDelete}><FiTrash2/></Button>
         </Form.Group>
+        </div>
     )
 
 }
