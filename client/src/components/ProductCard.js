647693eabf12fc56cf5aaf98f32647f240572dc3
angie <amaticorena103@hotmail.com>
2023-01-08 20:47:39 -0500

    add pop up to add cart button if user is not logged in

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+import { LoginContext } from './App';
 
 function ProductCard( {product, cartItems, setCartItems} ) {
 
+  const {currentUser} = useContext(LoginContext)
 
     function addToCart() {
+      if (currentUser) {
       const item = {
         product_id: product.id,
         quantity: 1
       }
       setCartItems([...cartItems, item])
-      };
+      } 
+    };
     
 
     return (
@@ -24,7 +30,12 @@ function ProductCard( {product, cartItems, setCartItems} ) {
           <Card.Text>
             ${product.price}
           </Card.Text>
+          { !currentUser ?
+          <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">You Must Login To Order</Tooltip>}>
             <Button onClick={addToCart} variant='dark'>Add To Cart</Button>
+            </OverlayTrigger> :
+          <Button onClick={addToCart} variant='dark'>Add To Cart</Button>
+          }
         </Card.Body>
       </Card>
       </Col>
